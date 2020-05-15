c60e5c416199ad04eaa03593c95d61825ef07430
Grace Hayden <graceehayden@gmail.com>
2020-05-15 13:57:50 -0500

    splash page added

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', views.coming_soon, name='coming_soon'),
     path('post_list', views.post_list, name='post_list'),
     path('hello_pallet', views.hello_pallet, name='hello_pallet'),
-]
+] + staticfiles_urlpatterns()
