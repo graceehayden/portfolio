5fa1bed38c5a821af7d58b0fbcd4aecf8ef48260
Grace Hayden <graceehayden@gmail.com>
2020-05-15 16:06:59 -0500

    resume added

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -5,5 +5,6 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('', views.coming_soon, name='coming_soon'),
     path('post_list', views.post_list, name='post_list'),
+    path('resume', views.resume, name='resume'),
     path('hello_pallet', views.hello_pallet, name='hello_pallet'),
 ] + staticfiles_urlpatterns()
