461e22c668cac7c0aab1977d7b7b3449fb3b4907
Grace Hayden <graceehayden@gmail.com>
2020-05-15 17:21:27 -0500

    removed key

--- gracestation/settings.py
+++ gracestation/settings.py
@@ -1,14 +1,8 @@
 import os
+from .local_settings import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4_=%623ms*!t4hq!&vz@5@2hajxp!2epy!97)#w9()o6w13p&!'
 
 DEBUG = True
 
