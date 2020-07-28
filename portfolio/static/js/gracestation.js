17729ff3160cc96cd9d1c8e0985b72631d7f4a04
Grace Hayden <graceehayden@gmail.com>
2020-07-28 10:50:33 -0500

    portfolio added

--- portfolio/static/js/gracestation.js
+++ portfolio/static/js/gracestation.js
@@ -1,6 +1,24 @@
+// Display of hover text
 function show(id) {
   document.getElementById(id).style.visibility = "visible";
 }
 function hide(id) {
   document.getElementById(id).style.visibility = "hidden";
+}
+
+
+// collapsible display of project details
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+coll[i].addEventListener("click", function() {
+  this.classList.toggle("active");
+  var content = this.nextElementSibling;
+  if (content.style.maxHeight){
+    content.style.maxHeight = null;
+  } else {
+    content.style.maxHeight = content.scrollHeight + "px";
+  }
+});
 }
