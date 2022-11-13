1e1618445accadede061d8213c82e06f242b7a17
Grace Hayden <gracehayden@Graces-MBP.home>
2022-11-13 11:47:45 +0100

    restarting

--- static/js/coming_soon.js
+++ static/js/coming_soon.js
@@ -1,5 +1,4 @@
-// Set the date we're counting down to
-var countDownDate = new Date("September 1, 2020 12:00:00").getTime();
+var countDownDate = new Date("September 1, 2023 12:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
